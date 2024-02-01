94627f64f9e7b0d3603a54c056ae6bd3021ed256
Petru99Ch <117940325+Petru99Ch@users.noreply.github.com>
2024-02-01 20:46:05 +0200

    v2

--- src/layout/restaurant/Menu.js
+++ src/layout/restaurant/Menu.js
@@ -1,15 +1,45 @@
 import Item from "./Item"
 import "./Menu.css"
 
-const Menu = (props) =>{
-    let data = props.data    
+import { useState } from "react"
+
+const Menu = ({data}) =>{
+
+    let [itemsData, setItemsData] = useState(data) 
+
+    const sortItemDesc = () =>{
+        itemsData.sort((i1, i2) => i2.price - i1.price)
+        setItemsData([...itemsData])
+
+        
+    }
+
+    const sortItemAsc = () =>{
+        itemsData.sort((i1, i2) => i1.price - i2.price)
+        setItemsData([...itemsData])
+
+        
+    }
+
+
+    let [val, setDesc] = useState(false)
+    const sortUniversal = () =>{     
+        setDesc(!val)
+        val ?  sortItemDesc() : sortItemAsc()        
+    }
+       
         return(
-            <ul className="menu">
-                <Item data ={data[0]} />
-                <Item data ={data[1]} />
-                <Item data ={data[2]} />
-                <Item data ={data[3]} />
-            </ul>
+            <div className="wrapper">
+                <div className="button">
+                    <button onClick={sortItemDesc}>Descend</button> 
+                    <button onClick={sortItemAsc}>Ascend</button> 
+                    <button onClick={sortUniversal} className={val ? "Desc" : "Asce"}></button>
+                </div>
+                <ul className="menu">
+                {itemsData.map(itemData => <Item key ={itemData.id} data = {itemData} />)}   
+                    
+                </ul>
+            </div>
         )
    
    
